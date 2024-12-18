04373f5975a1481deec51b68b0acc85f0ead3d7a
guhls <78971590+guhls@users.noreply.github.com>
2024-12-18 20:59:35 +0000

    Updated index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ function Home() {
         <li>"Vou comprar um cachorro ... --Maria, Luciana"</li>
         <li>"Lei nº 96.701/2024 - Art. 1º É proibida a produção e consumo de brigadeiro sem o processo de enrolar e aplicação de granulado em todo o território nacional."</li>
         <li>"May the force with be you --Francisco, Papa"</li>
-        <li>"num gosto -- Beatriz, Ana"</li>
+        <li>"num gosto -- Ana, beatriz"</li>
     </ul>
 }
 
