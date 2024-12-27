b11278e4d8dca7f4f0ff8e48f3a52537c71814ec
guhls <78971590+guhls@users.noreply.github.com>
2024-12-27 15:16:37 +0000

    configura o prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # categoriad.com.br
+
 Esse pedaço da internet contém uma plataforma dedicada a criar soluções para prestadores de serviços que trabalham com veículos
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 // Componente React
 function Home() {
-    return <ul>
-        <li>"Vou comprar um cachorro ... --Maria, Luciana"</li>
-        <li>"Lei nº 96.701/2024 - Art. 1º É proibida a produção e consumo de brigadeiro sem o processo de enrolar e aplicação de granulado em todo o território nacional."</li>
-        <li>"May the force with be you --Francisco, Papa"</li>
-        <li>"num gosto -- Ana, beatriz"</li>
+  return (
+    <ul>
+      <li>"Vou comprar um cachorro ... --Maria, Luciana"</li>
+      <li>
+        "Lei nº 96.701/2024 - Art. 1º É proibida a produção e consumo de
+        brigadeiro sem o processo de enrolar e aplicação de granulado em todo o
+        território nacional."
+      </li>
+      <li>"May the force with be you --Francisco, Papa"</li>
+      <li>"num gosto -- Ana, beatriz"</li>
     </ul>
+  );
 }
 
 export default Home;
